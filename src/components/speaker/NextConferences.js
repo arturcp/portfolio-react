c3861ae178d682a92f1bcf7dd6d1bf4c7c8f0a9a
Artur <artur.prado@gmail.com>
2019-11-06 16:32:43 -0300

    Add status to conference

--- src/components/speaker/NextConferences.js
+++ src/components/speaker/NextConferences.js
@@ -16,30 +16,34 @@ class NextConferences extends React.Component {
   }
 
   buildConference(conference, index) {
-    return (
-      <div className="pure-g" key={`conference-${index}`}>
-        <Calendar month={conference.date.month} day={conference.date.day} weekDay={conference.date.weekDay} />
+    if (conference.status === "pending") {
+      return (
+        <div className="pure-g" key={`conference-${index}`}>
+          <Calendar month={conference.date.month} day={conference.date.day} weekDay={conference.date.weekDay} />
 
-        <div className="pure-u-sm-1-1 pure-u-md-3-5 pure-u-lg-4-5">
-          <p className="first-paragraph">
-            {conference.name}
-          </p>
+          <div className="pure-u-sm-1-1 pure-u-md-3-5 pure-u-lg-4-5">
+            <p className="first-paragraph">
+              {conference.name}
+            </p>
 
-          <p className="first-paragraph">
-            {conference.talk}
-          </p>
+            <p className="first-paragraph">
+              {conference.talk}
+            </p>
 
-          {conference.paragraphs.map((paragraph, paragraphIndex) => {
-            return <p key={`paragraph-${index}-${paragraphIndex}`}>{paragraph}</p>
-          })}
+            {conference.paragraphs.map((paragraph, paragraphIndex) => {
+              return <p key={`paragraph-${index}-${paragraphIndex}`}>{paragraph}</p>
+            })}
 
-          <Promo conference={conference} />
-          <Location location={conference.icons.location} />
-          <Website website={conference.icons.website} />
-          <Tickets tickets={conference.icons.tickets} />
+            <Promo conference={conference} />
+            <Location location={conference.icons.location} />
+            <Website website={conference.icons.website} />
+            <Tickets tickets={conference.icons.tickets} />
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return null;
+    }
   }
 
   render () {
