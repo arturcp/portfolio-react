06c96220f6021f76da54178f5fd8eb21f909665a
Artur <artur.prado@gmail.com>
2019-11-06 16:40:36 -0300

    Create conference component

---
+++ src/components/speaker/Conference.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import Calendar from "./conference/Calendar";
+import Icon from "./conference/Icon";
+import Promo from "./conference/Promo";
+import Location from "./conference/Location";
+import Website from "./conference/Website";
+import Tickets from "./conference/Tickets";
+
+class Conference extends React.Component {
+  render() {
+    let conference = this.props.conference;
+
+    if (conference.status === "pending") {
+      return (
+        <div className="pure-g">
+          <Calendar month={conference.date.month} day={conference.date.day} weekDay={conference.date.weekDay} />
+
+          <div className="pure-u-sm-1-1 pure-u-md-3-5 pure-u-lg-4-5">
+            <p className="first-paragraph">
+              {conference.name}
+            </p>
+
+            <p className="first-paragraph">
+              {conference.talk}
+            </p>
+
+            {conference.paragraphs.map((paragraph, paragraphIndex) => {
+              return <p key={`paragraph-${conference-name}-${paragraphIndex}`}>{paragraph}</p>
+            })}
+
+            <Promo conference={conference} />
+            <Location location={conference.icons.location} />
+            <Website website={conference.icons.website} />
+            <Tickets tickets={conference.icons.tickets} />
+          </div>
+        </div>
+      )
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Conference;
--- src/components/speaker/NextConferences.js
+++ src/components/speaker/NextConferences.js
@@ -1,54 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Calendar from "./conference/Calendar";
-import Icon from "./conference/Icon";
-import Promo from "./conference/Promo";
-import Location from "./conference/Location";
-import Website from "./conference/Website";
-import Tickets from "./conference/Tickets";
-
+import Conference from "./Conference";
 import data from '../data/conferences.json';
 
 class NextConferences extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  buildConference(conference, index) {
-    if (conference.status === "pending") {
-      return (
-        <div className="pure-g" key={`conference-${index}`}>
-          <Calendar month={conference.date.month} day={conference.date.day} weekDay={conference.date.weekDay} />
-
-          <div className="pure-u-sm-1-1 pure-u-md-3-5 pure-u-lg-4-5">
-            <p className="first-paragraph">
-              {conference.name}
-            </p>
-
-            <p className="first-paragraph">
-              {conference.talk}
-            </p>
-
-            {conference.paragraphs.map((paragraph, paragraphIndex) => {
-              return <p key={`paragraph-${index}-${paragraphIndex}`}>{paragraph}</p>
-            })}
-
-            <Promo conference={conference} />
-            <Location location={conference.icons.location} />
-            <Website website={conference.icons.website} />
-            <Tickets tickets={conference.icons.tickets} />
-          </div>
-        </div>
-      )
-    } else {
-      return null;
-    }
-  }
-
   render () {
-    const items = data.conferences.map((conference, _index) =>
-      this.buildConference(conference)
+    const items = data.conferences.map((conference, index) =>
+      <Conference conference={conference} key={`conference-${index}`}/>
     );
 
     return(
