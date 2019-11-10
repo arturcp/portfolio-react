d2d9fcb6d1bbe948dd27bd3ab90444d948c81e8b
Artur <artur.prado@gmail.com>
2019-11-10 17:12:13 -0300

    Update years on biography

--- src/components/developer/About.js
+++ src/components/developer/About.js
@@ -10,7 +10,7 @@ class About extends React.Component {
       <Collapsible id="about" title="About" defaultChecked="checked">
         <SocialMedia />
         <p>
-          13 years eating, breathing and dreaming code. The first five years were dedicated to C# .Net,
+          15 years eating, breathing and dreaming code. The first five years were dedicated to C# .Net,
           and then I shifted my career to Ruby on Rails and have been flying on it since then. It's been an amazing ride.
         </p>
       </Collapsible>
